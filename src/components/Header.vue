b77080ac98d047211bac00cdae00f915c15b4e00
JImmy <59650459+okesseko@users.noreply.github.com>
2022-05-31 21:49:04 +0800

    feat: header component

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  addDataToList: Function,
+});
+
+const text = ref("");
+
+const add = () => {
+  if (!text.value.trim()) return;
+
+  props.addDataToList({
+    id: Date.now(),
+    text: text.value,
+    isCompleted: false,
+  });
+  text.value = "";
+};
+</script>
+
+<template>
+  <input v-model="text" @keyup.enter="add" placeholder="輸入代辦事項" />
+</template>
+
+<style scoped>
+input {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 4px 8px;
+  border: 2px solid #a17c6b;
+  border-radius: 4px;
+  font-size: 16px;
+  outline: none;
+}
+</style>
