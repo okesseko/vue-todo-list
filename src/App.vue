cc4c3083f76e8403833011e1c6e5f749fd231ef7
JImmy <59650459+okesseko@users.noreply.github.com>
2022-05-31 21:48:54 +0800

    feat: update template to todo list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import List from "./components/List.vue";
+import Footer from "./components/Footer.vue";
+import { reactive, ref } from "vue";
+
+let listData = reactive([
+  { id: 1, text: "吃飯", isCompleted: false },
+  { id: 2, text: "打掃", isCompleted: true },
+  { id: 3, text: "喝水", isCompleted: false },
+]);
+
+const addDataToList = (data) => {
+  listData.unshift(data);
+};
+
+const removeData = (index) => {
+  console.log("remove");
+  listData.splice(index, 1);
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Header :addDataToList="addDataToList" />
+  <List :list="listData" :removeData="removeData" />
+  <Footer />
 </template>
 
 <style>
+body {
+  background-color: #e0f2e9;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #ceb5a7;
+
+  width: 360px;
+  margin: 60px auto;
+
+  border: 2px solid #a17c6b;
+  border-radius: 8px;
+  padding: 24px 36px;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #1f2421;
 }
 </style>
